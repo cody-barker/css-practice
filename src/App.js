8cc3bc0f15a27babbd920ec79b5c0a794ed4bab9
Cody <codybarker.or@gmail.com>
2023-12-02 12:55:44 -0800

    add generated bakery page and css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,39 @@ import React from 'react';
 function App() {
   return (
     <>
-      <div class="yellow-div">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      </div>
-      <div class="blue-div"></div>
-    </>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#menu">Menu</a>
+        <a href="#contact">Contact</a>
+      </nav>
+
+      <section class="hero" id="home">
+        <h1>Delicious Bakery</h1>
+        <p>Freshly baked goods made with love</p>
+      </section>
+
+      <section class="menu" id="menu">
+        <div class="menu-item">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Korb_mit_Br%C3%B6tchen.JPG/1200px-Korb_mit_Br%C3%B6tchen.JPG" alt="Bread"/>
+          <h2>Bread</h2>
+          <p>Our freshly baked bread is made with the finest ingredients.</p>
+        </div>
+        <div class="menu-item">
+          <img src="https://cakebycourtney.com/wp-content/uploads/2015/08/Cookies-and-Cream-Cake-4-e1563420600943.jpg" alt="Cakes"/>
+          <h2>Cakes</h2>
+          <p>Indulge in our delicious cakes, perfect for any occasion.</p>
+        </div>
+
+      </section>
+
+
+      <section class="contact" id="contact">
+        <h2>Contact Us</h2>
+        <p>Visit us at 123 Bakery Street, Cityville</p>
+        <p>Email: info@deliciousbakery.com</p>
+        <p>Phone: (555) 123-4567</p>
+      </section>
+  </>
   )
 }
 
